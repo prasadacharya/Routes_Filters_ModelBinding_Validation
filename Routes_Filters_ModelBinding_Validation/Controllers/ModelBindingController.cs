a73c3fcc5948b9fd029acd7f78e8b375868ce715
prasadacharya <prasada@mail.usf.edu>
2020-04-11 00:44:29 -0400

    .

--- Routes_Filters_ModelBinding_Validation/Controllers/ModelBindingController.cs
+++ Routes_Filters_ModelBinding_Validation/Controllers/ModelBindingController.cs
@@ -37,5 +37,18 @@ namespace Routes_Filters_ModelBinding_Validation.Controllers
     public ViewResult NamesList(IList<string> names) => View(names ?? new List<string>());
 
     public ViewResult Address(IList<AddressSummary> addresses) => View(addresses ?? new List<AddressSummary>());
-  }
+
+        public ViewResult bindExample()
+        {
+            Course course = new Course();
+
+            return View(course);
+        }
+
+        [HttpPost]
+        public ViewResult bindExample(Course course)
+        {
+            return View(course);
+        }
+    }
 }
---
+++ Routes_Filters_ModelBinding_Validation/Models/CourseModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routes_Filters_ModelBinding_Validation.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Room { get; set; }
+    }
+}
